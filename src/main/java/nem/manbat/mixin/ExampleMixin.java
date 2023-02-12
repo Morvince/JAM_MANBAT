71263eb8a6d24d92385cfc94ef6239dec89e7e9e
Marvin <marvin.verain@epitech.eu>
2023-02-12 21:18:27 +0100

    [ADD]: adding manbat project sources

---
+++ src/main/java/nem/manbat/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package nem.manbat.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import nem.manbat.Manbat;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Manbat.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
